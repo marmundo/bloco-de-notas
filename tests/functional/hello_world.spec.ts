21652b5bd9db729cb1fc8fa56bcaefc48b46b379
Marcelo Damasceno <marcelodamasceno@gmail.com>
2022-12-04 21:40:48 -0300

    Delete hello_world.spec.ts

--- tests/functional/hello_world.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test } from '@japa/runner'
-import User from 'App/Models/User'
-
-test('sitema está online', async ({ client }) => {
-  const user = await User.find(1)
-  const response = await client.get('/api/notas').guard('api').loginAs(user)
-
-  response.assertStatus(200)
-})
